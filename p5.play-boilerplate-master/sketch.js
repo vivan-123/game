861779eefdd0693fe01c121397b0c9f302e9ef9c
vivan-123 <69099674+vivan-123@users.noreply.github.com>
2021-04-30 15:56:02 +0530

    Update sketch.js

--- p5.play-boilerplate-master/sketch.js
+++ p5.play-boilerplate-master/sketch.js
@@ -1,11 +1,12 @@
 var gamestate = "start"
 var canvas;
 var carrotsGroup;
-var carrot1,carrot2,carrot3,carrot4,carrot5,carrot6,carrot7,carrot8;
-
+var carCount = 0;
 
 //plant carrots button
 var plantCarrotsButton;
+
+
 
 
 
@@ -110,7 +111,62 @@ function setup() {
   plantCarrotsButton.visible = false;
 
   //carrots group
-    carrotsGroup = createGroup();
+    carrotsGroup = new Group();
+
+      //carrots
+      carrot1 = createSprite(475,220);
+      carrot1.addImage(carrotImg);
+      carrot1.scale = 0.2;
+      carrot1.visible = false;
+      carrotsGroup.add(carrot1);
+  
+  
+      carrot2 = createSprite(475,325);
+      carrot2.addImage(carrotImg);
+      carrot2.scale = 0.2;
+      carrot2.visible = false;
+      carrotsGroup.add(carrot2);
+  
+      carrot3 = createSprite(650,220);
+      carrot3.addImage(carrotImg);
+      carrot3.scale = 0.2;
+      carrot3.visible = false;
+      carrotsGroup.add(carrot3);
+  
+      carrot4 = createSprite(650,325);
+      carrot4.addImage(carrotImg);
+      carrot4.scale = 0.2;
+      carrot4.visible = false;
+      carrotsGroup.add(carrot4);
+  
+      carrot5 = createSprite(750,220);
+      carrot5.addImage(carrotImg);
+      carrot5.scale = 0.2;
+      carrot5.visible = false;
+      carrotsGroup.add(carrot5);
+  
+      carrot6 = createSprite(750,325);
+      carrot6.addImage(carrotImg);
+      carrot6.scale = 0.2;
+      carrot6.visible = false;
+      carrotsGroup.add(carrot6);
+  
+      carrot7 = createSprite(950,220);
+      carrot7.addImage(carrotImg);
+      carrot7.scale = 0.2;
+      carrot7.visible = false;
+      carrotsGroup.add(carrot7);
+  
+      carrot8 = createSprite(950,325);
+      carrot8.addImage(carrotImg);
+      carrot8.scale = 0.2;
+      carrot8.visible = false;
+      carrotsGroup.add(carrot8);
+
+      harvestButton = createSprite(600,700);
+      harvestButton.addImage(plantCarrotsButtonImg);
+      harvestButton.scale = 0.3;
+      harvestButton.visible = false;
 }
 
 function draw() {
@@ -154,55 +210,13 @@ function draw() {
 
     if (mousePressedOver(plantCarrotsButton))
     {
-      
-      carrot1 = createSprite(475,220);
-      carrot1.addImage(carrotImg);
-      carrot1.scale = 0.2;
-      //carrot1.visible = false;
-      carrotsGroup.add(carrot1);
-  
-  
-      carrot2 = createSprite(475,325);
-      carrot2.addImage(carrotImg);
-      carrot2.scale = 0.2;
-      //carrot2.visible = false;
-      carrotsGroup.add(carrot2);
-  
-      carrot3 = createSprite(650,220);
-      carrot3.addImage(carrotImg);
-      carrot3.scale = 0.2;
-      //carrot3.visible = false;
-      carrotsGroup.add(carrot3);
-  
-      carrot4 = createSprite(650,325);
-      carrot4.addImage(carrotImg);
-      carrot4.scale = 0.2;
-      //carrot4.visible = false;
-      carrotsGroup.add(carrot4);
-  
-      carrot5 = createSprite(750,220);
-      carrot5.addImage(carrotImg);
-      carrot5.scale = 0.2;
-      //carrot5.visible = false;
-      carrotsGroup.add(carrot5);
-  
-      carrot6 = createSprite(750,325);
-      carrot6.addImage(carrotImg);
-      carrot6.scale = 0.2;
-      //carrot6.visible = false;
-      carrotsGroup.add(carrot6);
-  
-      carrot7 = createSprite(950,220);
-      carrot7.addImage(carrotImg);
-      carrot7.scale = 0.2;
-      //carrot7.visible = false;
-      carrotsGroup.add(carrot7);
-  
-      carrot8 = createSprite(950,325);
-      carrot8.addImage(carrotImg);
-      carrot8.scale = 0.2;
-      //carrot8.visible = false;
-      carrotsGroup.add(carrot8);
+      carrotsGroup.get(carCount).visible=true;
+      carCount++;
+    }
+
+    if (carrot8.visible === true)
+    {
+      harvestButton.visible = true;
     }
 
   }
